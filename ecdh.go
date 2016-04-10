83ec35ddcac5837095f30b7c98872295a9f95235
Raffaele Meyer <rafmeyer91@gmail.com>
2016-04-10 16:52:55 +0200

    First commit

---
+++ ecdh.go
@@ -0,0 +1,40 @@
+package echd
+
+import (
+	"crypto/elliptic"
+	"crypto/rand"
+	"errors"
+)
+
+
+var ErrPointNotOnCurve = errors.New("The specified public key contains a point which isn't on the curve.")
+
+type ECDH struct {
+    Curve elliptic.Curve
+    PublicKey []byte
+    PrivateKey []byte
+}
+
+func GenerateNew(curve elliptic.Curve) (*ECDH, error)  {
+    ecdh := &ECDH{ Curve: curve }
+    priv, x, y, err := elliptic.GenerateKey(curve, rand.Reader)
+    if err != nil {
+        return nil, err
+    }
+    ecdh.PrivateKey = priv
+    ecdh.PublicKey = elliptic.Marshal(curve, x, y)
+    return ecdh, nil
+}
+
+func (ecdh *ECDH) GetSharedSecret(otherPub []byte) ([]byte, error)  {
+    pk := otherPub
+    px, py := elliptic.Unmarshal(ecdh.Curve, pk)
+    if px == nil {
+        return nil, ErrPointNotOnCurve
+    }
+    
+    sx, sy := ecdh.Curve.ScalarMult(px, py, ecdh.PrivateKey)
+    
+    return elliptic.Marshal(ecdh.Curve, sx, sy), nil
+}
+
